2f59d3a9f520560d31311730ed9aa904a5f3548b
lancelot.hero.op@gmail.com <lancelot.hero.op@gmail.com>
2025-03-19 21:48:48 +0700

    acara10

---
+++ database/seeds/DetailProfileSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DetailProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // insert data ke table pegawai
+        DB::table('detail_profile')->insert([
+            'address' => 'Jember',
+            'nomor_tlp'=> '08xxxxxxx',
+            'ttl'=> '2000-11-26',
+            'foto'=> 'picture.png',
+        ]);
+    }
+}
